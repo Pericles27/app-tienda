518e666211bc2bdd52a485821b82556a1b117b7b
Nicolás <nicolasherrera231@gmail.com>
2022-07-06 16:57:55 -0300

    firebase

--- src/components/ItemDetailContainer/ItemDetailConteiner.js
+++ src/components/ItemDetailContainer/ItemDetailConteiner.js
@@ -2,6 +2,8 @@ import { useEffect, useState} from "react"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import productos from '../../utils/prodMock'
 import { useParams, useNavigate } from "react-router-dom"
+import { doc, getDoc } from 'firebase/firestore'
+import db from "../../utils/firebaseConfig"
 
 const ItemDetailContainer = () => {
     const { id } = useParams()
@@ -9,17 +11,24 @@ const ItemDetailContainer = () => {
     const [product , setProduct] = useState({})
 
     useEffect(() => {
-        console.log("productFilter: ", productFilter)
-        if(productFilter === undefined){
-            navigate('/notFound')
-        }else {
-            setProduct(productFilter)
-        }
+        
+        getProduct()
+        .then( (prod) => {
+            console.log("Respuesta getProduct: ", prod)
+            setProduct(prod)
+        })
     }, [id])
 
-    const productFilter = productos.find( (product) => {
-        return product.id == id
-    })
+
+    const getProduct = async() => {
+        const docRef = doc(db, "productos", id)
+        const docSnaptshop = await getDoc(docRef)
+        console.log("docSnaptshop: ", docSnaptshop)
+        let product = docSnaptshop.data()
+        product.id = docSnaptshop.id
+        console.log("producto unico: ", product)
+        return product
+    }
 
     return(
         <>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,32 +4,53 @@ import { useState, useEffect } from 'react';
 import productos from '../../utils/prodMock';
 import { useParams, useNavigate } from 'react-router-dom'
 
+import db from '../../utils/firebaseConfig';
+import { collection, getDocs, query, where } from "firebase/firestore";
+
 const CardList = ({title}) => {
     const [products, setProducts] = useState([])
     const { category } = useParams()
 
+
+
+
     useEffect( () => {
         setProducts([])
-//         getProducts()
-        // .then( (response) => {
-            category ? filterByCategory(productos, category) : setProducts(productos)
-        // })
+        console.log()
+        getProducts()
+        .then( (productos) => {
+            category ?  filterFirebase() : setProducts(productos)
+        })
     }, [category])
 
-    
-    // const getProducts = () => {
-    //     return new Promise( (resolve, reject) => {
-    //             resolve(productos)
-    //     })
-    // }  
-
-    const filterByCategory = (array, category) => {
-        return array.map( (item) => {
-            if(item.category === category) {
-                return setProducts(products => [...products, item])
-            }
+    const getProducts = async () => {
+        const productCollection = collection(db, "productos")
+        const productSnapshot = await getDocs(productCollection);
+        const productList = productSnapshot.docs.map((doc) => {
+            let product = doc.data()
+            product.id = doc.id
+            return product
         })
+        return productList
     }
+
+    const filterFirebase = async () => {
+        const productRef = collection(db, 'productos')
+        const queryResult = query(productRef, where("category", "==", category));
+        const querySnapshot = await getDocs(queryResult);
+        const productList = querySnapshot.docs.map((doc) => {
+            let product = doc.data()
+            product.id = doc.id
+            return product
+        })
+        return setProducts(productList)
+    }
+
+
+
+
+
+
     return(
         <>
         <h2>{title}</h2>
